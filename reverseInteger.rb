522cb18dc5a30302a9ef6c9fdaeaf43b00bfa060
adifsgaid <infosgaid@gmail.com>
2021-04-01 20:39:37 +0200

    reverse integer solution

---
+++ reverseInteger.rb
@@ -0,0 +1,10 @@
+# @param {Integer} x
+# @return {Integer}
+def reverse(x)
+   if x.negative?
+    x =  (x.to_s.reverse.to_i * - 1)
+  else
+    x = (x.to_s.reverse.to_i)
+  end
+  x.bit_length > 31 ? 0 : x
+end
