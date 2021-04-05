6dfac2a532d26b3752d88e657a7d091e3d5cf2f1
adifsgaid <infosgaid@gmail.com>
2021-04-05 16:22:38 +0200

    Grid Traveller using Recursion and Memoization

--- gridTraveler.rb
+++ gridTraveler.rb
@@ -5,3 +5,13 @@ def grid_traveler(x,y)
   return 0 if x == 0 || y == 0
   grid_traveler(x - 1,y) + grid_traveler(x,y - 1)
 end
+
+
+def grid_traveler(x, y, memo={})
+  key = x,y
+  return memo[key] if memo.include?(key)
+  return 1 if x == 1 && y == 1
+  return 0 if x == 0 || y == 0
+  memo[key] = grid_traveler(x - 1, y, memo) + grid_traveler(x, y - 1, memo)
+  return memo[key]
+end
