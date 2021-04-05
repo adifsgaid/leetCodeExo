731f2688c8140c8f1c36d48a9f6589528b1ee16b
adifsgaid <infosgaid@gmail.com>
2021-04-05 15:56:33 +0200

    grid traveller using RECURSION

--- gridTraveler.rb
+++ gridTraveler.rb
@@ -1,7 +1,7 @@
 # Grid Traveller using RECURSION
 
 def grid_traveler(x,y)
-return 1 if x == 1 && y == 1
-return 0 if x == 0 || y == 0
-grid_traveler(x - 1,y) + grid_traveler(x,y - 1)
+  return 1 if x == 1 && y == 1
+  return 0 if x == 0 || y == 0
+  grid_traveler(x - 1,y) + grid_traveler(x,y - 1)
 end
