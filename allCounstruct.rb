8f9be3d20804f5fc08c66883fc2ab875b55a5e25
adifsgaid <infosgaid@gmail.com>
2021-04-09 18:40:29 +0200

    Algo solution all contsruct

--- allCounstruct.rb
+++ allCounstruct.rb
@@ -25,3 +25,17 @@ all_contruct("purple",["purp","p","ur","le","purpl"])
 ["p","ur", "p", "le"]
 ]
 
+
+def all_contruct( target, wordBank)
+  nextPossibleWords = wordBank.select{|word| target.start_with? word}
+  nextPossibleWords.each_with_object([]) do |word,result|
+    rest = target.delete_prefix word
+    if rest.empty?
+      result << [word]
+    else
+      all_contruct(rest,wordBank).each {|contruct| result << [word] + contruct}
+    end
+  end
+end
+
+all_contruct("purple",["purp","p","ur","le","purpl"])
