53109d6c30f3d6976a8f054ee1d9de26b47875e4
adifsgaid <infosgaid@gmail.com>
2021-04-21 12:15:23 +0200

    memoization can contruct

--- canConstruct.rb
+++ canConstruct.rb
@@ -38,12 +38,12 @@ can_construct(["eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef"],["e","ce","ee","eeeeeeee
 def can_construct(target, wordBank)
   return true if target == ''
 
-  result =  wordBank.select{|word| target.start_with? word}
+  result =  wordBank.select{|word| target.index(word)}
 
   result.each  do |word|
 
     rest = target.delete_prefix word
-    suffix = rest.slice!(word.length)
+    suffix = rest.slice(word.length)
 
     if can_construct(suffix, wordBank) == true
       return true
@@ -53,11 +53,27 @@ def can_construct(target, wordBank)
 end
 
 
+def can_construct(target, wordBank, memo = {})
+  return memo[target] if memo.include?(target)
+  return true if target == ''
+
+  wordBank.each do |word|
+    if target.index(word) == 0
+      suffix =  target.slice(word.length)
+      if can_construct(suffix, wordBank, memo)
+        memo[target] = true
+        return true
+      end
+    end
+  end
+  memo[target] = false
+  return false
+end
 
 
 
 
 
-
-can_construct(["abc"],["a","cd","c","b"])
-can_construct(["abcdef"],["a","cd","c","b"])
+can_construct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef",["e","ce","ee","eeeeeeee"])
+can_construct("abc",["a","cd","c","b"])
+can_construct("abcdef",["a","cd","c","b"])
