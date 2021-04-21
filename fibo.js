1019341b5a9b118d1f233aadab881594bb36cd53
adifsgaid <infosgaid@gmail.com>
2021-04-21 17:05:54 +0200

    Fibo tabo

--- fibo.js
+++ fibo.js
@@ -1,7 +1,15 @@
 const fibo = (n) => {
   const table = Array(n + 1).fill(0);
   table[1] = 1;
-  console.log(table)
+
+  for (let i = 0; i <= n; i++){
+      table[i + 1] += table[i]
+      table[i + 2] += table[i]
+    }
+    return table[n]
 };
 
 console.log(fibo(6))
+console.log(fibo(7))
+console.log(fibo(8))
+console.log(fibo(50))
