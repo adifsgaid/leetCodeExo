7633d9783c4a15ee93b12eddcab29b26eb787bd9
adifsgaid <infosgaid@gmail.com>
2021-04-22 15:09:08 +0200

    first com

--- fibonacciTabulazione.rb
+++ fibonacciTabulazione.rb
@@ -1,7 +1,15 @@
 def fibo_tabu(n)
-  @table = Array.new(n + 1).fill(0);
-  @table[1]=1
-  return @table
+  table = Array.new(n + 1).fill(0);
+  table[1]= 1
+  i = 0
+   while i < n do
+    table[i + 1] += table[i]
+    table[i + 2] += table[i]
+   end
+ end
+  table[n]
 end
+
 fibo_tabu(4)
+
 
