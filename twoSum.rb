80500c3bcd2a50edcbea980bbe0b0622a44bd791
adifsgaid <infosgaid@gmail.com>
2021-04-26 12:37:00 +0200

    first comm

--- twoSum.rb
+++ twoSum.rb
@@ -10,3 +10,18 @@ def two_sum(nums, target)
   end
   search
 end
+nums = [2,7,11,15], target = 9
+
+
+
+def two_sum(target, nums)
+
+  nums.each_with_index do |num, index|
+      current = target - num
+      return nil if current.nil?
+      result = two_sum(current, nums)
+      result = index
+  end
+  result
+end
+two_sum(9,[2,7,11,15])
