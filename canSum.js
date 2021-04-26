b2138a72aca148e8761a6e7affe9b829e0f63d38
adifsgaid <infosgaid@gmail.com>
2021-04-26 12:57:33 +0200

    Algo can sum tabulation

--- canSum.js
+++ canSum.js
@@ -4,6 +4,17 @@ const canSum = (target, numbers) => {
   }
   const table = Array(target + 1).fill(false);
   table[0] = true;
-   console.log(table)
+
+  for(let i = 0; i <= target; i++ ){
+    if (table[i] === true){
+      for(let num of numbers){
+        table[i + num] = true
+      }
+    }
+  }
+  return table[target]
 }
-console.log(canSum(4,[2,3,4]))
+console.log(canSum(7,[2,4]))
+console.log(canSum(7,[5,3,4,7]))
+console.log(canSum(8,[2,3,5]))
+console.log(canSum(300,[7,14]))
