109edc70f6347a124b9b88aa9cff4c274b5a5a1c
adifsgaid <infosgaid@gmail.com>
2021-04-27 12:00:18 +0200

    algo test

--- howSumTabulation.js
+++ howSumTabulation.js
@@ -1,6 +1,14 @@
 const howSumTabulation = (target, numbers) => {
   const table =  Array(target + 1).fill(null)
   table[0] = []
-  console.log(table)
+
+  for (let i = 0; i <= target; i++){
+    if (table[i] !==  null ){
+     for (let num of  numbers){
+      table[i + num] = [...table[i]]
+     }
+    }
+  }
+  return table[target]
 }
 console.log(howSumTabulation(5,[4,5,3]))
