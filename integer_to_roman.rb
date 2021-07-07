e421f8422cb00d80d7c06ca6c6eef7f1121234d3
adifsgaid <infosgaid@gmail.com>
2021-07-07 13:05:30 +0200

    integer_to_roman

---
+++ integer_to_roman.rb
@@ -0,0 +1,12 @@
+def integer_to_roman(num)
+  digits_to_roman = { 1000 => 'M', 900 => 'CM', 500 => 'D', 400 => 'CD', 100 => 'C', 90 => 'XC', 50 => 'L', 40 => 'XL', 10 => 'X', 9 => 'IX', 5 => 'V', 4 => 'IV', 1 => 'I' }
+  roman = ''
+  digits_to_roman.each do |key, value|
+    if num >= key
+      times = num / key
+      roman += value * times
+      num -= key * times
+    end
+  end
+  roman
+end
