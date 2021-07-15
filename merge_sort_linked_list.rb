b97f60eb17dae1e9437642922407c375a3d4f9e0
adifsgaid <infosgaid@gmail.com>
2021-07-15 14:27:22 +0200

    O(n) linear time complexity

--- merge_sort_linked_list.rb
+++ merge_sort_linked_list.rb
@@ -7,3 +7,14 @@ def merge_sort_linked_list(l1, l2)
   l1.next = merge_sort_linked_list(l1.next, l2)
   l1
 end
+
+def merge_Sort(l1, l2)
+  return l1 || l2 unless l1 && l2
+
+  if l2.val > l1.val
+    l1.next = merge_two_lists(l1.next, l2)
+    l1
+  elsif l2.next = merge_two_lists(l1, l2.next)
+    l2
+  end
+end
