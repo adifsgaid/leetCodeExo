e897ee0a2da86efa025dacf0eb3c82e9e6e0c8fe
adifsgaid <infosgaid@gmail.com>
2021-07-15 14:04:31 +0200

    merge sort algo

---
+++ merge_sort_linked_list.rb
@@ -0,0 +1,9 @@
+def merge_sort_linked_list(l1, l2)
+  return l1 if l2.nil?
+  return l2 if l1.nil?
+
+  l1, l2 = l2, l1 if l1.val? > l2.val?
+
+  l1.next = merge_sort_linked_list(l1.next, l2)
+  l1
+end
