7c039dc5f41409ff49d288409d88319660828e63
adifsgaid <infosgaid@gmail.com>
2021-07-16 15:02:50 +0200

    merge sort

--- binary_search.rb
+++ binary_search.rb
@@ -5,13 +5,37 @@ def binary_search(nums, target)
   while left <= right
     mid = (left + right) / 2
 
-    return mid if nums[mid] == target
-
-    if nums[mid] > target
-      right = mid - 1
+    if nums[mid] == target
+      return mid
+    elsif nums[mid] > target
+       right = mid - 1
     elsif nums[mid] < target
-      left  = mid + 1
+      left = mid + 1
     end
   end
   -1
 end
+search([3, 4, 3, 2, 1], 9)
+
+def search(nums, target)
+  left = 0
+  right = nums.length - 1
+  
+  while left <= right 
+      mid = (left + right) / 2
+      
+      if nums[mid] == target
+          return mid
+      
+      elsif nums[mid] < target
+          left = mid + 1
+      
+      elsif nums[mid] > target
+          
+          right = mid  - 1
+      end
+  end
+  -1
+end
+
+search([-1,0,3,5,9,12], 9)
