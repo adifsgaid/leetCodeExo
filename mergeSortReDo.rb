50e462e96aa3de7b951cff23862060378b775d47
adifsgaid <infosgaid@gmail.com>
2021-07-20 11:49:44 +0200

    merge sort

---
+++ mergeSortReDo.rb
@@ -0,0 +1,23 @@
+def merge_sort(arr)
+  return arr if arr.length <= 1
+
+  mid = (arr.length / 2).floor
+  left = merge_sort(arr[0..mid - 1])
+  right = merge_sort(arr[mid..arr.length])
+  merge(left, right)
+end
+
+def merge(left, right)
+  if left.empty?
+    right
+  elsif right.empty?
+    left
+  elsif left.first < right.first
+    [left.first]  + merge(left[1..left.length], right)
+  else
+    [right.first] + merge(left, right[1..right.length])
+  end
+end
+arr = [2, 434, 434, 12, 33, 2212, 4343]
+p merge_sort(arr)
+
