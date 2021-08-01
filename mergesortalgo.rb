07f4e266058c04e171486aea5188a46357f2496c
adifsgaid <infosgaid@gmail.com>
2021-08-01 14:40:15 +0200

    added algo

---
+++ mergesortalgo.rb
@@ -0,0 +1,13 @@
+def merge_sort(arr); end
+
+def merge(left, right)
+  if left.empty?
+    right
+  elsif right.empty?
+    left
+  elsif left.first < right.first
+    [left.first] + merge(left[1..left.length], right)
+  else
+    [right.first] + merge(left, right[1..right.length])
+  end
+end
